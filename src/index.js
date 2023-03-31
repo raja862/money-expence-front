230259e3b3d0c0e6de81e928b232ac8988e22497
N Rajakumaran <108915966+raja862@users.noreply.github.com>
2023-03-31 12:53:01 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { GlobalProvider } from './context/globalContext';
+import { GlobalStyle } from './styles/GlobalStyle';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <GlobalStyle />
+    <GlobalProvider>
+      <App />
+    </GlobalProvider>
+  </>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
